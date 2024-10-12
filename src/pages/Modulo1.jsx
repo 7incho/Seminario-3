c1c7baf479ea66c0616413bfd6b5382809f80b22
7incho <martin30990@gmail.com>
2024-10-12 20:35:33 -0300

    maqueta modulo 1 y 2

--- src/pages/Modulo1.jsx
+++ src/pages/Modulo1.jsx
@@ -2,34 +2,34 @@ import React from 'react';
 
 function Modulo1() {
   return (
-    <div>
-      <div className='grid grid-cols-3 gap-4 h-auto'>
-        <div className='col-span-2 bg-blue-300 p-4 rounded-lg'>
+    <div className="h-full">
+      <div className="grid grid-cols-3 gap-4 h-full">
+        <div className="col-span-2 bg-blue-300 p-4 rounded-lg flex items-center justify-center">
           <h2 className="text-lg font-semibold">Buen día, Alejandra</h2>
         </div>
-        <div className='bg-blue-400 p-4 rounded-lg'>
+        <div className="bg-blue-300 p-4 rounded-lg flex items-center justify-center">
           <h2 className="text-lg font-semibold">HH:MM</h2>
         </div>
 
-        <div className='bg-blue-300 p-4 rounded-lg'>
+        <div className="bg-blue-300 p-4 rounded-lg flex items-center justify-center">
           Promedio general del curso
         </div>
-        <div className='bg-blue-400 p-4 rounded-lg'>
+        <div className="bg-blue-300 p-4 rounded-lg flex items-center justify-center">
           Porcentaje de asistencia
         </div>
-        <div className='bg-blue-500 p-4 rounded-lg'>
+        <div className="bg-blue-300 p-4 rounded-lg flex items-center justify-center">
           Estadísticas
         </div>
 
-        <div className='bg-blue-300 p-4 rounded-lg'>
+        <div className="bg-blue-300 p-4 rounded-lg flex items-center justify-center">
           Porcentaje de estudiantes en riesgo
         </div>
-        <div className='col-span-2 row-span-2 bg-blue-500 p-4 rounded-lg'>
+        <div className="col-span-2 row-span-2 bg-blue-400 p-4 rounded-lg flex flex-col justify-between">
           <h2 className="text-lg font-semibold">Chat Edutrack</h2>
           <p className="mt-2">Hola Alejandra!...</p>
         </div>
 
-        <div className='bg-blue-500 p-4 rounded-lg'>
+        <div className="bg-blue-300 p-4 rounded-lg flex items-center justify-center">
           Cuadro
         </div>
       </div>
--- src/pages/Modulo2.jsx
+++ src/pages/Modulo2.jsx
@@ -1,12 +1,40 @@
-import React from 'react'
+import React from 'react';
 
 function Modulo2() {
- return (
- <div>
-    <h1 className="text-2xl font-bold">Vista Particular</h1>
-    <p>Esta la vista particular.</p>
- </div>
- )
+  return (
+    <div className="h-full">
+      <div className="grid grid-cols-3 gap-4 h-full">
+        <div className="col-span-2 bg-blue-300 p-4 rounded-lg flex items-center justify-center">
+          <h2 className="text-lg font-semibold">Buen día, Alejandra</h2>
+        </div>
+        <div className="bg-blue-300 p-4 rounded-lg flex items-center justify-center">
+          <h2 className="text-lg font-semibold">HH:MM</h2>
+        </div>
+
+        <div className="bg-blue-300 p-4 rounded-lg flex items-center justify-center">
+          Promedio general del curso
+        </div>
+        <div className="bg-blue-300 p-4 rounded-lg flex items-center justify-center">
+          Porcentaje de asistencia
+        </div>
+        <div className="bg-blue-300 p-4 rounded-lg flex items-center justify-center">
+          Estadísticas
+        </div>
+
+        <div className="bg-blue-300 p-4 rounded-lg flex items-center justify-center">
+          Porcentaje de estudiantes en riesgo
+        </div>
+        <div className="col-span-2 row-span-2 bg-blue-400 p-4 rounded-lg flex flex-col justify-between">
+          <h2 className="text-lg font-semibold">Chat Edutrack</h2>
+          <p className="mt-2">Hola Alejandra!...</p>
+        </div>
+
+        <div className="bg-blue-300 p-4 rounded-lg flex items-center justify-center">
+          Cuadro
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Modulo2
+export default Modulo2;
